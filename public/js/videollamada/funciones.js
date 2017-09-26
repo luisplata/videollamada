c028c78770d054387ee9f0689a78b1218d7a4ee8
Tilson Fernandez <fernandeztilson@gmail.com>
2017-09-26 16:56:08 -0500

    Probando videollamada validación peers

--- public/js/videollamada/funciones.js
+++ public/js/videollamada/funciones.js
@@ -34,10 +34,10 @@ console.log(webrtc);
 // when it's ready, join if we got a room from the URL
 webrtc.on('readyToCall', function () {
 
-        console.log("Entrando al room");
+        
         // you can name it anything
         if (room){ 
-            if(webrtc.getPeers.length > 2){
+            if(obtener_numero_de_usuarios(webrtc) == 2){
                 //alert("Sala llena");
 
                 console.log("Sala llena =(");
@@ -46,6 +46,7 @@ webrtc.on('readyToCall', function () {
 
             }else{
                 webrtc.joinRoom(room);
+                console.log("Entrando al room");
             }
             
         }else{
@@ -391,6 +392,7 @@ function desconectar(){
 
 function obtener_numero_de_usuarios(webrtc){
     var n_user = webrtc.webrtc.peers.length;
+    console.log("# peers: "+ n_user);
 
     return n_user;
 }
